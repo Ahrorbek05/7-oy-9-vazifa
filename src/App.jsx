e03f4787ec1977427a2a3d5e8f568ad841edb88a
Ahrorbek05 <akramjonovahrorbek96@gmail.com>
2024-08-30 16:38:53 +0500

    home work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './component/Navbar'
 import Trending from './component/Trending'
 import Recomended from './component/Recomended'
 import './App.css'
+import SavedMovies from './SavedMovies'
 
 function App() {
   return (
-    <div className='container'>
-      <Trending></Trending>
-    <div className="container flex gap-8 mx-auto w-[1280px]">
-      <Navbar></Navbar>
-      <Recomended></Recomended>
-    </div>
-    </div>
+    <Router>
+      <div className='container'>
+        <Trending />
+        <div className="container flex gap-8 mx-auto w-[1280px]">
+          <Navbar />
+          <Recomended />
+          <Routes>
+            <Route path='/SavedMovies' element={<SavedMovies />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   )
 }
 
